838c3530fddce76ddc59c5b3ad9e3e97355aa99c
maxrawlinger <max.rawlinger@googlemail.com>
2022-06-28 16:18:34 +0200

    chore: remove unused dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import '@/styles/globals.css';
 import { AppProps } from 'next/app';
-import { HubspotProvider } from '@aaronhayes/react-use-hubspot-form';
 import { NinetailedProvider } from '@ninetailed/experience.js-next';
 import { NinetailedPreviewPlugin } from '@ninetailed/experience.js-plugin-preview';
 
 const B2BDemoApp = ({ Component, pageProps }: AppProps) => {
   return (
     <div className="app">
-      <HubspotProvider>
-        <NinetailedProvider
-          plugins={[
-            NinetailedPreviewPlugin({
-              clientId:
-                process.env.NEXT_PUBLIC_NINETAILED_MANAGEMENT_CLIENT_ID ?? '',
-              secret:
-                process.env.NEXT_PUBLIC_NINETAILED_MANAGEMENT_SECRET ?? '',
-              environment:
-                process.env.NEXT_PUBLIC_NINETAILED_ENVIRONMENT ?? 'main',
-              ui: { opener: { hide: false } },
-            }),
-          ]}
-          clientId={process.env.NEXT_PUBLIC_NINETAILED_CLIENT_ID ?? ''}
-          environment={process.env.NEXT_PUBLIC_NINETAILED_ENVIRONMENT ?? 'main'}
-        >
-          <Component {...pageProps} />
-        </NinetailedProvider>
-      </HubspotProvider>
+      <NinetailedProvider
+        plugins={[
+          NinetailedPreviewPlugin({
+            clientId:
+              process.env.NEXT_PUBLIC_NINETAILED_MANAGEMENT_CLIENT_ID ?? '',
+            secret: process.env.NEXT_PUBLIC_NINETAILED_MANAGEMENT_SECRET ?? '',
+            environment:
+              process.env.NEXT_PUBLIC_NINETAILED_ENVIRONMENT ?? 'main',
+            ui: { opener: { hide: false } },
+          }),
+        ]}
+        clientId={process.env.NEXT_PUBLIC_NINETAILED_CLIENT_ID ?? ''}
+        environment={process.env.NEXT_PUBLIC_NINETAILED_ENVIRONMENT ?? 'main'}
+      >
+        <Component {...pageProps} />
+      </NinetailedProvider>
     </div>
   );
 };
