126e2cf4017c15bdb5a77be17373c344b73eed4d
maxrawlinger <max.rawlinger@googlemail.com>
2022-06-28 15:45:00 +0200

    feat: add form validation

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -2,16 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { useNinetailed, useProfile } from '@ninetailed/experience.js-next';
 import { Field, Form as ContactForm } from 'react-final-form';
 import * as Yup from 'yup';
+import { makeValidate } from '@/lib/Validation';
 
-const defaultErrorMessage = 'Please complete this required field';
 export const Form: React.FC = () => {
   const { profile } = useProfile();
   const { identify } = useNinetailed();
   const [showForm, setShowForm] = useState<boolean>(true);
-
-  type ErrorType = {
-    [key: string]: unknown;
-  };
 
   type FormTraits = {
     [key: string]: string;
@@ -35,50 +31,14 @@ export const Form: React.FC = () => {
     setShowForm(false);
   };
 
-  /* const validationSchema = Yup.object({
-    firstName: Yup.string().required(),
-    lastName: Yup.string().required(),
-    companyName: Yup.string().required(),
-    companySize: Yup.string().required(),
-    businessEmail: Yup.string().email(),
-  }); */
-  /* type FormValidationSchema = Yup.InferType<typeof validationSchema>;
-  const validateFormValues = (schema: FormValidationSchema) => {
-    return async (values: any) => {
-      /!* if (typeof schema === 'function') {
-        schema = schema();
-      } *!/
-      try {
-        await schema.validate(values, { abortEarly: false });
-      } catch (err) {
-        const errors = err.inner.reduce((formError, innerError) => {
-          return setIn(formError, innerError.path, innerError.message);
-        }, {});
-
-        return errors;
-      }
-    };
-  };
-
-  const validate = validateFormValues(validationSchema);
-  const validate = async (values) => {
-    try {
-      await validationSchema.validate(values, { abortEarly: false });
-    } catch (err: unknown) {
-      console.log(err.inner);
-      /* return err.inner.reduce(
-        (formError: any, innerError: { path: any; message: any }) => {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-          return {
-            ...formError,
-            [innerError.path]: innerError.message,
-          };
-        },
-        {}
-      );
-    }
-  }} */
-
+  const validationSchema = Yup.object({
+    'First Name': Yup.string().required(),
+    'Last Name': Yup.string().required(),
+    'Company Name': Yup.string().required(),
+    'Company Size': Yup.string().ensure().required(),
+    'Business Email': Yup.string().email().required(),
+  });
+  const validate = makeValidate(validationSchema);
   return (
     <>
       <div
@@ -89,29 +49,8 @@ export const Form: React.FC = () => {
         {showForm ? (
           <ContactForm
             onSubmit={onSubmit}
-            validate={(values) => {
-              const errors = {} as ErrorType;
-              if (!values.firstname) {
-                errors.firstname = defaultErrorMessage;
-              }
-              if (!values.lastname) {
-                errors.lastname = defaultErrorMessage;
-              }
-              if (!values.companyName) {
-                errors.companyName = defaultErrorMessage;
-              }
-              if (
-                !values.companySize ||
-                values.companySize === 'defaultValue'
-              ) {
-                errors.companySize = defaultErrorMessage;
-              }
-              if (!values.businessEmail) {
-                errors.businessEmail = defaultErrorMessage;
-              }
-              return errors;
-            }}
-            initialValues={{ companySize: 'defaultValue' }}
+            validate={validate}
+            initialValues={{ 'Company Size': '' }}
             render={({ handleSubmit }) => {
               return (
                 <form
@@ -119,16 +58,11 @@ export const Form: React.FC = () => {
                   className="flex flex-col space-y-4 items-start"
                 >
                   <fieldset className="flex flex-row w-full justify-between">
-                    <Field
-                      name="firstName"
-                      validate={(value) => {
-                        return value ? undefined : 'Required';
-                      }}
-                    >
+                    <Field name="First Name">
                       {({ input, meta }) => {
                         return (
                           <div className="w-[48%] flex flex-col">
-                            <label htmlFor="firstName" className="text-[14px]">
+                            <label htmlFor="First Name" className="text-[14px]">
                               First Name
                               <span className="text-[#f2545b]">*</span>
                             </label>
@@ -147,11 +81,11 @@ export const Form: React.FC = () => {
                         );
                       }}
                     </Field>
-                    <Field name="lastName">
+                    <Field name="Last Name">
                       {({ input, meta }) => {
                         return (
                           <div className="w-[48%] flex flex-col">
-                            <label htmlFor="lastName" className="text-[14px]">
+                            <label htmlFor="Last Name" className="text-[14px]">
                               Last Name<span className="text-[#f2545b]">*</span>
                             </label>
                             <input
@@ -171,12 +105,12 @@ export const Form: React.FC = () => {
                     </Field>
                   </fieldset>
                   <fieldset className="flex flex-row w-full">
-                    <Field name="companyName">
+                    <Field name="Company Name">
                       {({ input, meta }) => {
                         return (
                           <div className="w-full flex flex-col">
                             <label
-                              htmlFor="companyName"
+                              htmlFor="Company Name"
                               className="text-[14px]"
                             >
                               Company name
@@ -200,22 +134,20 @@ export const Form: React.FC = () => {
                   </fieldset>
 
                   <fieldset className="flex flex-col w-full ">
-                    <label htmlFor="companySize" className="text-[14px]">
+                    <label htmlFor="Company Size" className="text-[14px]">
                       Company Size<span className="text-[#f2545b]">*</span>
                     </label>
-                    <Field name="companySize" placeholder="Select your option">
+                    <Field name="Company Size" placeholder="Select your option">
                       {({ input, meta }) => {
                         return (
                           <>
                             <select
                               {...input}
                               className={` border-2 bg-[#f5f8fa] h-[40px] px-[15px] focus:outline-indigo-600 border-[1px] rounded-[3px] border-[#cbd6e2] ${
-                                input.value === 'defaultValue'
-                                  ? 'text-gray-400'
-                                  : ''
+                                input.value === '' ? 'text-gray-400' : ''
                               }`}
                             >
-                              <option value="defaultValue" disabled>
+                              <option value="" disabled>
                                 Select your option
                               </option>
                               <option value="1-50">1-50</option>
@@ -238,12 +170,12 @@ export const Form: React.FC = () => {
                     </Field>
                   </fieldset>
                   <fieldset className="flex flex-row w-full">
-                    <Field name="businessEmail">
+                    <Field name="Business Email">
                       {({ input, meta }) => {
                         return (
                           <div className="w-full flex flex-col">
                             <label
-                              htmlFor="businessEmail"
+                              htmlFor="Business Email"
                               className="text-[14px]"
                             >
                               Business Email
---
+++ lib/Validation.ts
@@ -0,0 +1,123 @@
+import { ReactNode } from 'react';
+import {
+  AnySchema as YupSchema,
+  ValidationError as YupValidationError,
+} from 'yup';
+
+import { get, set } from 'lodash';
+
+/* // https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_get
+function get(obj: any, path: string, defaultValue?: any) {
+  const result = String.prototype.split
+    .call(path, /[,[\].]+?/)
+    .filter(Boolean)
+    .reduce((res, key) => {
+      return res !== null && res !== undefined ? res[key] : res;
+    }, obj);
+  return result === undefined || result === obj ? defaultValue : result;
+}
+
+// https://stackoverflow.com/questions/54733539/javascript-implementation-of-lodash-set-method
+function set(obj: any, path: any, value: any) {
+  if (Object(obj) !== obj) return obj; // When obj is not an object
+  // If not yet an array, get the keys from the string-path
+  if (!Array.isArray(path)) path = path.toString().match(/[^.[\]]+/g) || [];
+  path.slice(0, -1).reduce(
+    (
+      a: any,
+      c: any,
+      i: number // Iterate all of them except the last one
+    ) => {
+      return Object(a[c]) === a[c] // Does the key exist and is its value an object?
+        ? // Yes: then follow that path
+          a[c]
+        : // No: create the key. Is the next key a potential array-index?
+          (a[c] =
+            Math.abs(path[i + 1]) >> 0 === +path[i + 1]
+              ? [] // Yes: assign a new array object
+              : {});
+    }, // No: assign a new plain object
+    obj
+  )[path[path.length - 1]] = value; // Finally assign the value to the last key
+  return obj; // Return the top-level object to allow chaining
+} */
+
+export type Translator = (errorObj: YupValidationError) => string | ReactNode;
+
+export interface ValidationError {
+  [key: string]: ValidationError | string;
+}
+
+function normalizeValidationError(
+  err: YupValidationError,
+  translator?: Translator
+): ValidationError {
+  return err.inner.reduce((errors, innerError) => {
+    const { path, message } = innerError;
+    const el: ReturnType<Translator> = translator
+      ? translator(innerError)
+      : message;
+
+    // eslint-disable-next-line no-prototype-builtins
+    if (path && errors.hasOwnProperty(path)) {
+      const prev = get(errors, path);
+      prev.push(el);
+      set(errors, path, prev);
+    } else {
+      set(errors, path, [el]);
+    }
+    return errors;
+  }, {});
+}
+
+/**
+ * Wraps the execution of a Yup schema to return an Promise<ValidationError>
+ * where the key is the form field and the value is the error string.
+ */
+export function makeValidate<T>(
+  validator: YupSchema<T>,
+  translator?: Translator
+) {
+  return async (values: T): Promise<ValidationError> => {
+    try {
+      await validator.validate(values, { abortEarly: false });
+      return {};
+    } catch (err) {
+      return normalizeValidationError(err as YupValidationError, translator);
+    }
+  };
+}
+
+/**
+ * Wraps the sync execution of a Yup schema to return an ValidationError
+ * where the key is the form field and the value is the error string.
+ */
+export function makeValidateSync<T>(
+  validator: YupSchema<T>,
+  translator?: Translator
+) {
+  return (values: T): ValidationError => {
+    try {
+      validator.validateSync(values, { abortEarly: false });
+      return {};
+    } catch (err) {
+      return normalizeValidationError(err as YupValidationError, translator);
+    }
+  };
+}
+
+/**
+ * Uses the private _exclusive field in the schema to get whether or not
+ * the field is marked as required or not.
+ */
+export function makeRequired<T>(schema: YupSchema<T>) {
+  const { fields } = schema as any;
+  return Object.keys(fields).reduce((accu, field) => {
+    if (fields[field].fields) {
+      accu[field] = makeRequired(fields[field]);
+    } else {
+      accu[field] = !!fields[field].exclusiveTests.required;
+    }
+    return accu;
+  }, {} as any);
+}
